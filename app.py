e65e488788dfd72d6f053bc3bf4c0b77c51d6ef3
phw0228@gmail.com <phw0228@gmail.com>
2023-09-04 10:36:48 +0900

    google login test

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/static/css/idxsty.css')
+def static_file():
+    return send_file('static/css/idxsty.css')
+
+@app.route('/templates/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/templates/feed.html')
+def feed():
+    return render_template('feed.html')
+
 if __name__ == '__main__':
     app.run()
 
