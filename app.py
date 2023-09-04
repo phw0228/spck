7128e76bb691dc163f3c3b175ef8da2cd49962f9
phw0228@gmail.com <phw0228@gmail.com>
2023-09-04 16:39:03 +0900

    commit

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/static/css/idxsty.css')
-def static_file():
-    return send_file('static/css/idxsty.css')
-
 @app.route('/login.html')
 def login():
     return render_template('login.html')
@@ -21,6 +17,14 @@ def feed():
 @app.route('/ctrct.html')
 def ctrct():
     return render_template('ctrct.html')
+
+@app.route('/static/css/idxsty.css')
+def idxsty():
+    return send_file('static/css/idxsty.css')
+
+@app.route('/static/css/cntrsty.css')
+def cntrsty():0
+    return send_file('static/css/cntrsty.css')
 
 if __name__ == '__main__':
     app.run()
