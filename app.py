8fdb7e0739a9e59bc1140318754f788797efd23b
phw0228@gmail.com <phw0228@gmail.com>
2023-09-04 16:42:13 +0900

    commit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/static/css/idxsty.css')
+def static_file():
+    return send_file('static/css/idxsty.css')
+
 @app.route('/login.html')
 def login():
     return render_template('login.html')
@@ -16,14 +20,7 @@ def feed():
 
 @app.route('/ctrct.html')
 def ctrct():
-    return render_template('ctrct.html')
-
-@app.route('/static/css/idxsty.css')
-def idxsty():
-    return send_file('static/css/idxsty.css')
-
-@app.route('/static/css/cntrsty.css')
-def cntrsty():0
+    # 이 라인의 들여쓰기를 맞추어야 합니다.
     return send_file('static/css/cntrsty.css')
 
 if __name__ == '__main__':
