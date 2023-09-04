0dd547fb8f806fe8e238dcc0be1b7d37bc2488f0
phw0228@gmail.com <phw0228@gmail.com>
2023-09-04 13:24:09 +0900

    test

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route("/")
+#def homepage():
+#    if session.get('logged_in') :
+#        return render_template('loggedin.html')
+#    else:
+#        return render_template('index.html')
+#
 @app.route('/')
 def index():
     return render_template('index.html')
