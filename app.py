4046ee42c182857ca7d477eb2322be1649e0884e
phw0228@gmail.com <phw0228@gmail.com>
2023-09-15 14:41:16 +0900

    test

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
+from flask import Flask, render_template
+import pymysql
+
+app = Flask(__name__)
+
+def connect_db():
+    connection = pymysql.connect(
+        host="mysql-container",
+        user="root",
+        password="0228",
+        database="board"
+    )
+    return connection
+
+def get_rows():
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT name, age FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+@app.route("/")
+def index():
+    rows = get_rows()
+    return render_template("index.html", rows=rows)
+
 @app.route("/input", methods=["POST"])
 def input():
     name = request.form["name"]
@@ -9,3 +37,6 @@ def input():
     cursor.close()
     connection.close()
     return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
