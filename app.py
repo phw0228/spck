6f96ad9360815e43633535af8eb6302c2fff9486
phw0228@gmail.com <phw0228@gmail.com>
2023-09-15 12:17:49 +0900

    test

--- app.py
+++ app.py
@@ -1,57 +1,42 @@
 from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
 
-@app.route('/')
+def connect_db():
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="test"
+    )
+    return connection
+
+def get_rows():
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT name, age FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+@app.route("/")
 def index():
-    return render_template('index.html', data_list=data_list)
+    rows = get_rows()
+    return render_template("index.html", rows=rows)
 
-@app.route('/static/css/idxsty.css')
-def static_file():
-    return send_file('static/css/idxsty.css')
+@app.route("/input", methods=["POST"])
+def input():
+    name = request.form["name"]
+    age = request.form["age"]
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (name, age))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return redirect("/")
 
-@app.route('/login.html')
-def login():
-    return render_template('login.html')
-    
-@app.route('/write.html')
-def write():
-    return render_template('write.html')
-
-@app.route('/feed.html')
-def feed():
-    return render_template('feed.html')
-
-@app.route('/ctrct.html')
-def ctrct():
-    return send_file('static/css/cntrsty.css')
-
-@app.route('/write_ok.php', methods=['POST'])
-def write_ok():
-    # write_ok.php를 실행합니다.
-    return os.system('php write_ok.php')
-
-@app.route('/', methods=['POST'])
-def index_post():
-    title = request.form['title']
-    content = request.form['content']
-
-    # write.php를 실행합니다.
-    return os.system('php write.php')
-
-if __name__ == '__main__':
-    app.run()
-
-import pymysql
-
-db = pymysql.connect(host="localhost", user="root", passwd="0228", db="board", charset="utf8")
-cur = db.cursor()
-
-sql = "SELECT * from board"
-cur.execute(sql)
-
-data_list = cur.fetchall()
-
-print(data_list[0])
-print(data_list[1])
-print(data_list[2])
+if __name__ == "__main__":
+    app.run(debug=True)
